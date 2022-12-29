b6ede0ddfc57ccfe4de0f2b97fee3463111ef669
MaltVinegar <maltvinegarandchips@gmail.com>
2022-12-29 16:13:16 -0500

    Revert "guh"
    This reverts commit 70fa47113e6f3cc129061c33c05149e073b81144.

--- dependencies.sh
+++ dependencies.sh
@@ -11,7 +11,7 @@ export BYOND_MINOR=1588
 export RUST_VERSION=1.54.0
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.7.1
+export RUST_G_VERSION=0.6.0
 
 #node version
 export NODE_VERSION=14
@@ -21,7 +21,7 @@ export NODE_VERSION_PRECISE=14.16.1
 export SPACEMAN_DMM_VERSION=suite-1.7.1
 
 #auxmos version
-export AUXMOS_VERSION=1.1.2
+export AUXMOS_VERSION=1.0.1
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.10
